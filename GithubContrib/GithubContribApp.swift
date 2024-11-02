7f69d370eac44bb48768ce2399c2a39c1219fe83
Shashank Verma <shashank.verma2002@gmail.com>
2024-11-02 13:54:00 +0530

    Initial Commit

---
+++ GithubContrib/GithubContribApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubContribApp.swift
+//  GithubContrib
+//
+//  Created by Shashank on 02/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubContribApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GithubContribTests/GithubContribTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubContribTests.swift
+//  GithubContribTests
+//
+//  Created by Shashank on 02/11/24.
+//
+
+import Testing
+@testable import GithubContrib
+
+struct GithubContribTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
